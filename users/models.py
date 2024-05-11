44f40ec7c7bbf7b95a37c9bd21c85287ce54cddc
DESKTOP-T534JQD\Rinal <santiagorinaldi22@gmail.com>
2024-05-11 18:48:19 -0300

    Se termino CustomUserManager

--- users/models.py
+++ users/models.py
@@ -13,4 +13,5 @@ class CustomUserManager(UserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)        
         user.set_password(password)
-        
+        user.save(using=self._db )
+        return user 
